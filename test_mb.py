64aac5f2af96b64d135621820c0851dea05935f2
Mikhail Shkliaev <mikhail.shkliaev@live.ru>
2023-09-01 01:01:02 +0300

    вынес в отдельную функцию чтение файла

--- test_mb.py
+++ test_mb.py
@@ -33,37 +33,41 @@ def get_addresses(input_str: str) -> list:
     return sorted(addr_out)
 
 
-def start():
+def get_settings() -> tuple:
     addresses = ""
     baudrate = 9600
     databits = 8
     parity = "none"
     stopbits = 1
     timeout = 500
-
     with open("setting.txt", "r") as setf:
         for line in setf:
             if line.startswith("addresses: "):
                 addresses = get_addresses(line.split(": ")[1].rstrip("\n"))
             elif line.startswith("baudrate: "):
-                baudrate = int(line.split()[1])
+                baudrate = int(line.split(": ")[1])
             elif line.startswith("databits: "):
-                databits = int(line.split()[1])
+                databits = int(line.split(": ")[1])
             elif line.startswith("parity: "):
-                parity = line.split()[1]
+                parity = line.split(": ")[1]
             elif line.startswith("stopbits: "):
-                stopbits = int(line.split()[1])
+                stopbits = int(line.split(": ")[1])
             elif line.startswith("timeout: "):
-                timeout = int(line.split()[1])
+                timeout = int(line.split(": ")[1])
+    return addresses, baudrate, databits, parity, stopbits, timeout
 
+
+def start():
+    addresses = []
+    addresses, baudrate, databits, parity, stopbits, timeout = get_settings()
     if len(addresses) != 0:
         if len(addresses) > 1:
             words = ["Эти", "а", "ут", "ы"]
         else:
             words = ["Этот", "", "ет", ""]
         print(f"{words[0]} адрес{words[1]} буд{words[2]} проверен{words[3]}: {addresses}\n"
-            "со следующими натройками:\n"
-            f"{baudrate} : {databits} {parity} {stopbits}")
+              "со следующими натройками:\n"
+              f"{baudrate} : {databits}{parity}{stopbits}")
 
 
 if __name__ == '__main__':
